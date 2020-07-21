ed99dcc077ec3b26aa98080d77e75adea73d23ba
ahmedYasser32 <55816002+ahmedYasser32@users.noreply.github.com>
2020-07-21 15:44:17 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -11,12 +11,12 @@
  *
  * JS Standard: ESlint
  *
-*/
+ */
 
 /**
  * Define Global Variables
  *
-*/
+ */
 
 const NavList =document.getElementById("navbar__list");
 const sections= document.querySelectorAll('section');
@@ -27,52 +27,67 @@ const sections= document.querySelectorAll('section');
 
  * Begin Main Functions
  *
-*/
+ */
 
 // build the nav
- function buildnavBar() {
+function buildnavBar() {
 
- const LFrag = document.createDocumentFragment();
- for (const sec of sections) {
-  const secID = sec.id;
-  const newli = document.createElement('li');
-  newli.classList.add("menu__link");
-  newli.innerHTML=`<a href="#${secID}">${secID}</a>`;
-  LFrag.appendChild(newli);
-  //scroll into the wanted element
-  newli.addEventListener('click',function(e){
-   e.preventDefault();
-   sec.scrollIntoView({
-    behavior:"smooth"
-   });
-  });
+    const LFrag = document.createDocumentFragment();
+    for (const sec of sections) {
+        const secID = sec.id;
+        const newli = document.createElement('li');
+        newli.classList.add("menu__link");
+        newli.id=secID;
+        newli.innerHTML=`<a href="#${secID}">${secID}</a>`;
+        LFrag.appendChild(newli);
+
+        //scroll into the wanted element
+        newli.addEventListener('click',function(e){
+            e.preventDefault();
+            sec.scrollIntoView({
+                behavior:"smooth"
+
+            });
+        });
 
 
-  NavList.appendChild(LFrag);
+        NavList.appendChild(LFrag);
 
 
- }
+    }
 }
 
- //build the event to know when the user is on top of viewport and control the active  class
+//build the event to know when the user is on top of viewport and control the active  class
 function buildevents() {
-
-    sections.forEach(section => {
-
+    for (let i=0;i<sections.length;i++){
+      let  section=sections[i];
+      let link = document.getElementById(section.id);
         const entry = Math.floor(section.getBoundingClientRect().top);
-        
+
         section.classList.toggle('your-active-class',false)
+        link.classList.toggle('your-active-class',false);
+
         if (entry <280 && entry >= -280){
-            section.classList.add('your-active-class',true);
+            section.classList.toggle('your-active-class',true);
+            link.classList.toggle('your-active-class',true);
+
         }
-            })
-                  }
+    }
+}
 /**
  * End Main Functions
  * Begin Events
  *
-*/
+ */
 // Build navigation menu
 document.addEventListener('DOMContentLoaded', (buildnavBar()));
 //begin the events that set and remove active class and know which class you are scrolling on
 window.addEventListener("scroll", buildevents);
+
+
+
+
+
+
+
+
